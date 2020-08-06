6754cf3e0baa651abf6442116f519964c3251b5a
Eric_Lian <git@superexercisebook.com>
2020-08-06 16:38:20 +0800

    添加 log4net

---
+++ JustChat_Center/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace JustChat_Center
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += GlobalExceptionEventHandler;
+
+            var logRepo = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            BasicConfigurator.Configure(logRepo);
+
+            log.Info("Entering application.");
+
+            var bar = new Bar();
+
+            ThreadStart childref = bar.DoIt;
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+
+            log.Info("Exiting application.");
+
+            Console.ReadKey();
+        }
+
+        public static void GlobalExceptionEventHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            log.Fatal(e.ExceptionObject);
+        }
+    }
+
+    class Bar
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Bar));
+
+        public void DoIt()
+        {
+            log.Debug("Did it again!");
+            log.Fatal("test", new Exception());
+            throw new Exception();
+        }
+    }
+
+}
